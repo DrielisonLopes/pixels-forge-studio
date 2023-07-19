d7513d1164aa18bf6e0dff8fff818ebf336bc9ef
DrielisonLopes <drielisonl@gmail.com>
2023-07-19 13:24:08 +0200

    header icons and styles

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
+
 import styled from "styled-components";
+import { LuSearch } from "react-icons/lu";
+import { PiUserCircleFill, PiCaretDownBold } from "react-icons/pi";
 
 const NameIcon = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 30px;
+  
+  div {
+    display: flex;
+    align-items: center;
+    font-family: monospace;
+    background: #494949;
+    height: 100%;
+    margin: 0 4px;
+    padding: 6px 22px;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background ease 0.3s, border ease 0.3s;
+    
+    &:hover {
+      background: #562684;
+    }
   }
 `;
 
@@ -18,8 +38,13 @@ const Header = () => {
        height={60}
        />
       <NameIcon>
-        <div className="icon">🔍</div>
-        <div className="names">Krilin Carec</div>
+        <div className="icon"><LuSearch/></div>
+        <div className="rain">0.00 RAIN</div>
+        <div className="user">
+          <span className="userIcon"><PiUserCircleFill/></span>
+          <span>Krilin Carec</span>
+          <span><PiCaretDownBold/></span>
+        </div>
       </NameIcon>
     </div>
   );
