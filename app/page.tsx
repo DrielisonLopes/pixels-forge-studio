a4943e056587bdd18c8ee13e26018c3f36954dc8
Renan Lopes <renanlopescoder@gmail.com>
2023-07-27 20:32:16 +0000

    Update 22 files

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+
+import Header from "@/molecules/Header";
+import AboutGame from "@/molecules/AboutGame";
+import Footer from "@/atoms/Footer";
+import styles from "../styles/_home.module.scss";
+
+export default function HomePage() {
+  return (
+    <div>
+      <div className={styles.headerContainer}>
+        <Header />
+      </div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/images/cityTank/tank-bg.jpg"
+          alt="Background Tank Game"
+          layout="fill"
+        />
+      </div>
+      <AboutGame />
+      <Footer />
+    </div>
+  );
+}
