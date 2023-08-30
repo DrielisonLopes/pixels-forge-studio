03ec2f8aa5eb1dc6f3ccce3e8eaf4c9f396a5c98
DrielisonLopes <drielisonl@gmail.com>
2023-08-30 18:08:29 +0100

    Start Game Page

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -4,17 +4,24 @@ import GameList from "@/molecules/GameList/GameList";
 import Footer from "@/molecules/Footer/Footer";
 
 import styles from "../../styles/_home.module.scss";
-import CardGame from "@/molecules/CardGame/CardGame";
+import ImageCarousel from "@/molecules/ImageCarousel/ImageCarousel";
 import Header from "@/molecules/Header/Header";
 
 export default function GamePage() {
+  const images = [
+    "/images/thumb-video.png",
+    "/images/cityTank/tank-bg.jpg",
+    "/images/tank2.jpeg",
+    "/images/cityTank/city-tank1.png",
+    "/images/cityTank/city-tank2.png",
+    "/images/fate-samurai-remnant.jpg",
+    "/images/tank.jpg",
+  ];
+
   return (
     <div className={styles.pageContainer}>
       <Header/>
-      <br />
-      <br />
-      <br />
-      <GameList />
+      <ImageCarousel images={images} />
       <Footer />
     </div>
   );
