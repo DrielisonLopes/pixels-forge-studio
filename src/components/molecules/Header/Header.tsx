847d32896f0945c0a063f16f4af4600fe46469fb
DrielisonLopes <drielisonl@gmail.com>
2023-09-25 20:37:32 +0100

    build Login Modal

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import Link from 'next/link';
-import { PiUserCircleFill, PiCaretDownBold } from "react-icons/pi";
+import { useState } from "react";
+import { PiUserCircleFill } from "react-icons/pi";
+import LoginModal from "../LoginModal/LoginModal";
 
 import styles from "./Header.module.scss";
 
 const Header = () => {
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
@@ -26,14 +39,15 @@ const Header = () => {
         </Link>
       </div>
       <div className={styles.nameIcon}>
-        <div className={styles.user}>
+        <div className={styles.user} onClick={handleLoginClick}>
           <span className={styles.userIcon}>
             <PiUserCircleFill />
           </span>
           <span>Login</span>
         </div>
-        <div className={styles.rain}>Sign up</div>
+        <div className={styles.rain} onClick={handleLoginClick}>Sign up</div>
       </div>
+      {isModalOpen && <LoginModal onClose={handleCloseModal} />}
     </div>
   );
 };
